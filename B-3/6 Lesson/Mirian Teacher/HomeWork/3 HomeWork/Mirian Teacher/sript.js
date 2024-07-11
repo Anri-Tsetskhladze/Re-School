0186e8f802ccde5f06e6c4f6c5418ea14882a0fc
Anri Tsetskhladze <tsetskhladzeanri209@gmail.com>
2024-07-12 00:36:41 +0400

    Delete B-3 directory

--- B-3/6 Lesson/Mirian Teacher/HomeWork/3 HomeWork/Mirian Teacher/sript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const today = new Date().getDay(); 
-    const headers = document.querySelectorAll('#schedule thead th');
-    
-    if (today > 0) { 
-        headers[today].style.backgroundColor = '#FF5722';
-    }
-});
--- B-3/8 Lesson/Mirian Teacher/Homework/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
-    cards.forEach((card, index) => {
-        setTimeout(() => {
-            card.classList.add('fade-in-up');
-        }, index * 200); 
-    });
-});
